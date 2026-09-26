c0780a58c2e25b360b24db753d97b8998297afb2
AdriMunoz1 <adrian.munoz.girona@gmail.com>
2026-09-26 15:48:41 +0200

    Motor actualizado para recibir DEJAR DE REGAR

--- src/WM_WS_M.py
+++ src/WM_WS_M.py
@@ -26,67 +26,60 @@ def recibir_utf(sock):
 
 def hilo_cliente_central(MENSAJE, ADDR):
     global sock_central
-    try:
-        cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        cliente.connect(ADDR)
-        print(f"Establecida conexión en [{ADDR}]")
+            cliente.connect(ADDR)
+            print(f"Establecida conexión en [{ADDR}]")
 
-        print("Envio al servidor: ", MENSAJE)
-        enviar_utf(cliente, MENSAJE)
-        respuesta = recibir_utf(cliente)
-        print("Recibo del Servidor: ", respuesta)
+            print("Envio al servidor: ", MENSAJE)
+            enviar_utf(cliente, MENSAJE)
+            respuesta = recibir_utf(cliente)
+            print("Recibo del Servidor: ", respuesta)
 
-        if "OK" in respuesta:
-            print("[WM_WS_M] Validación correcta en Central. Manteniendo conexión con [WM_Central]")
-            sock_central = cliente
-            # Mantenemos conexión con WM_Central para notificar estado 
-            # de WS y actualizar estado en la bbdd
-            while True:
-                mensaje = recibir_utf(cliente)
-                print(f"Mensaje: {mensaje}")
-                if not mensaje:
-                    print("[WM_WS_M] Conexión cerrada por Central.")
-                    break
-                print(f"[WM_WS_M] Mensaje recibido de Central: {mensaje}")
+            if "OK" in respuesta:
+                print("[WM_WS_M] Validación correcta en Central. Manteniendo conexión con [WM_Central]")
+                sock_central = cliente
+                # Mantenemos conexión con WM_Central para notificar estado 
+                # de WS y actualizar estado en la bbdd
+                while True:
+                    mensaje = recibir_utf(cliente)
+                    print(f"Mensaje: {mensaje}")
+                    if not mensaje:
+                        print("[WM_WS_M] Conexión cerrada por Central.")
+                        break
+                    print(f"[WM_WS_M] Mensaje recibido de Central: {mensaje}")
 
-        else:
-            print("[WM_WS_M] No se pudo validar en Central. Abortando arranque.")
+            else:
+                print("[WM_WS_M] No se pudo validar en Central. Abortando arranque.")
+                cliente.close()
+                sock_central = None
+
+        except Exception as e:
+            print(f"[WM_WS_M] Error en canal con [WM_Central]: {e}")
+        finally:
             cliente.close()
             sock_central = None
-
-    except Exception as e:
-        print(f"[WM_WS_M] Error en canal con [WM_Central]: {e}")
-    finally:
-        cliente.close()
-        sock_central = None
+    time.sleep(3)
 
 def splitear(mensaje):
     tramos = mensaje.split("#")
     return tramos
 
-# Hace de servidor con WM_WS_E
-def server(IP_WS, PORT_WS, MENSAJE):
-    
-    global sock_central
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((IP_WS, PORT_WS))
-
-    s.listen(1)
-    print(f"[WM_WS_M] Esperando conexión del Engine en el puerto {PORT_WS}...") 
-    conn, addr = s.accept() 
-    print(f"[WM_WS_M] Engine conectado desde {addr}")
-
-    try:
+def atender_engine(conn, MENSAJE):
+    try:    
         while True:
             conn.sendall("PING_HEALTH".encode(FORMAT))
 
             respuesta = conn.recv(HEADER).decode(FORMAT)
-            
+                
             if not respuesta:
                 print("[WM_WS_M] ERROR: El Engine se ha desconectado inesperadamente")
+                if sock_central:
+                    tramo = splitear(MENSAJE)
+                    enviar_utf(sock_central, f"ALERT#{tramo[1]}#DESCONECTADA")
                 break
             elif respuesta == "KO":
                 print("[WM_WS_M] ALERTA: El Engine ha sufrido un error (fuga/avería)")
@@ -98,6 +91,19 @@ def server(IP_WS, PORT_WS, MENSAJE):
                 else:
                     print("[WM_WS_M] No hay conexión con Central; alerta no enviada.")
                 break
+            elif respuesta == "RIEGO":
+                print("[WM_WS_M] STATUS: [WM_WS_E] ha activado el riego")
+                if sock_central:
+                    tramo = splitear(MENSAJE)
+                    mensaje = "CONNECT#"+tramo[1]+"#REGANDO"
+                    enviar_utf(sock_central, mensaje)
+                    print("[WM_WS_M]: Está en modo riego")
+            elif respuesta == "DEJAR_REGAR":
+                if sock_central:
+                    tramo = splitear(MENSAJE)
+                    mensaje = "CONNECT#"+tramo[1]+"#DISPONIBLE"
+                    enviar_utf(sock_central, mensaje)
+                    print("[WM_WS_M]: vuelve a estar disponible")
             else:
                 print(f"[WM_WS_M] Health Check: {respuesta}")
 
@@ -105,10 +111,36 @@ def server(IP_WS, PORT_WS, MENSAJE):
     except (OSError, ConnectionError) as e:
         print(f"Respuesta: {respuesta}")
         print(f"Error en la conexión con el Engine: {e}")
+        tramo = splitear(MENSAJE)
+        mensaje = "ALERT#"+tramo[1]+"#DESCONECTADA"
+        enviar_utf(sock_central, mensaje)
     finally:
-        conn.close()
-        s.close()
+        conn.close();
         print("Conexión cerrada con [WM_WS_E]")
+
+# Hace de servidor con WM_WS_E
+def server(IP_WS, PORT_WS, MENSAJE):
+    
+    global sock_central
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((IP_WS, PORT_WS))
+
+    s.listen(1)
+    partes = splitear(MENSAJE)
+    try:
+        while True:
+            print(f"[WM_WS_M] Esperando conexión del Engine en el puerto {PORT_WS}...") 
+            conn, addr = s.accept()
+            enviar_utf(sock_central, f"CONNECT#{partes[1]}#DISPONIBLE")
+            print(f"[WM_WS_M] Engine conectado desde {addr}")
+            
+            # Hacemos que se quede en otro bucle while para que siga atendiendo peticiones de WM_WS_E 
+            # después de haber cerrado una conexión con este
+            atender_engine(conn, MENSAJE)
+    finally:
+        s.close()
 
 
 def main():
@@ -121,17 +153,22 @@ def main():
         IP_WS = sys.argv[4]
         PORT_WS = int(sys.argv[5])
 
-        # Hilo donde hace de cliente con el servidor WM_Central
-        t_client = threading.Thread(target=hilo_cliente_central, args=(MENSAJE, ADDR))
-        t_client.start()
+        try:
+            # Hilo donde hace de cliente con el servidor WM_Central
+            t_client = threading.Thread(target=hilo_cliente_central, args=(MENSAJE, ADDR))
+            t_client.start()
 
-        time.sleep(1)
-        
-        t_server = threading.Thread(target=server, args=(IP_WS, PORT_WS, MENSAJE))
-        #server(IP_WS, PORT_WS, MENSAJE)
-        t_server.start()
-        #time.sleep(1) 
+            time.sleep(1)
+            
+            t_server = threading.Thread(target=server, args=(IP_WS, PORT_WS, MENSAJE))
+            
+            t_server.start()
+            #time.sleep(1) 
 
+            t_client.join()
+            t_server.join()
+        except KeyboardInterrupt:
+            print("[WM_WS_M] Cierre solicitado (Ctrl+C)")
     else:
         print("ERROR: Se necesitan mas argumentos: <ServerIP> <Port> <Argumentos> <IP_WS> <PORT_WS>")
 
